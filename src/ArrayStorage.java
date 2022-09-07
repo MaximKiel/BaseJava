69074134e288265de176cdb4b954c15b1a3efdad
MaximKiel <loup.du.nord@yandex.ru>
2022-09-07 20:33:57 +0300

    implement lesson_1 HW_1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,54 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int count = 0;
 
     void clear() {
+        for (int i = 0; i < count; i++) {
+            storage[i] = null;
+        }
+        count = 0;
     }
 
     void save(Resume r) {
+        storage[count] = r;
+        count++;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < count; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < count; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                System.arraycopy(storage, i + 1, storage, i, count);
+                count--;
+                return;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
+        if (count > 0) {
+            return Arrays.copyOfRange(storage, 0, count);
+        }
         return new Resume[0];
     }
 
     int size() {
-        return 0;
+        return count;
     }
 }
