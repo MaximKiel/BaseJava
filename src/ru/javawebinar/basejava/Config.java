435e686ad1f53b9d316abc464d8101f8cf497b41
MaximKiel <loup.du.nord@yandex.ru>
2023-01-01 20:36:06 +0300

    implement lesson_13

--- src/ru/javawebinar/basejava/Config.java
+++ src/ru/javawebinar/basejava/Config.java
@@ -1,14 +1,15 @@
 package ru.javawebinar.basejava;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 public class Config {
 
-    private static final Config INSTANCE = new Config();
     private static final File PROPERTIES = new File("config\\resumes.properties");
+    private static final Config INSTANCE = new Config();
     private Properties properties = new Properties();
     private File storageDir;
 
@@ -21,7 +22,7 @@ public class Config {
     }
 
     private Config() {
-        try (InputStream inputStream = getClass().getResourceAsStream("config\\resumes.properties")){
+        try (InputStream inputStream = new FileInputStream(PROPERTIES)){
             properties.load(inputStream);
             storageDir = new File(properties.getProperty("storage.dir"));
         } catch (IOException e) {
